be0f8817a889eaaa3d4a80c598de9e5afe083a4a
Mike Green <m.d.j.green@ntlworld.com>
2021-03-14 18:27:41 +0000

    Adding artwork query on title or description

--- artworks/views.py
+++ artworks/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Artworks
+from django.contrib import messages
+from django.db.models import Q
 
 
 def all_artworks(request):
     """ A view to show all artworks, including sorting and search queries """
 
     artworks = Artworks.objects.all()
+    qry = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            qry = request.GET['q']
+            if not qry:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('artworks'))
+                
+            queries = Q(title__icontains=qry) | Q(description__icontains=qry)
+            artworks = artworks.filter(queries)
 
     context = {
         'artworks': artworks,
+        'search_term': qry,
     }
 
     return render(request, 'artworks/artworks.html', context)
