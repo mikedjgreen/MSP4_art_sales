81afaf7ffe8ef30f3828aba5a72f15031dc01ca5
Mike Green <m.d.j.green@ntlworld.com>
2021-03-14 09:38:52 +0000

    Added artworks and artwork details page

--- artworks/views.py
+++ artworks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Artworks
 
 
@@ -12,3 +12,15 @@ def all_artworks(request):
     }
 
     return render(request, 'artworks/artworks.html', context)
+
+
+def artwork_detail(request, artwork_id):
+    """ A view to show individual artwork details """
+
+    artwork = get_object_or_404(Artworks, pk=artwork_id)
+
+    context = {
+        'artwork': artwork,
+    }
+
+    return render(request, 'artworks/artwork_detail.html', context)
