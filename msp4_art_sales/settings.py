6cdadf7f28727f93db526a459ad0147bd611b613
Mike Green <m.d.j.green@ntlworld.com>
2021-03-14 11:26:05 +0000

    TEMPORARILY setting DEBUG to True

--- msp4_art_sales/settings.py
+++ msp4_art_sales/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['msp4-art-sales.herokuapp.com', 'localhost']
 
