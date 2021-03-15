d6dd40b68943368f61bc86eb266890a273d1692e
Mike Green <m.d.j.green@ntlworld.com>
2021-03-15 09:06:11 +0000

    Added artwork sorts and more details

--- artworks/views.py
+++ artworks/views.py
@@ -9,20 +9,34 @@ def all_artworks(request):
 
     artworks = Artworks.objects.all()
     qry = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            artworks = artworks.order_by(sortkey)
+
         if 'q' in request.GET:
             qry = request.GET['q']
             if not qry:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(request, "You didn't enter any search criteria")
                 return redirect(reverse('artworks'))
                 
             queries = Q(title__icontains=qry) | Q(description__icontains=qry)
             artworks = artworks.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'        
+
     context = {
         'artworks': artworks,
         'search_term': qry,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'artworks/artworks.html', context)
