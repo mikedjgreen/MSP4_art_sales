cff6d3868bc613e81e25943937e27babd2334c7c
Mike Green <m.d.j.green@ntlworld.com>
2021-03-18 23:37:25 +0000

    Updated allauth templates

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Patron
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+            'user',
+            'default_phone_number',
+            'default_country',
+            'default_postcode',
+            'default_town_or_city',
+            'default_street_address1',
+            'default_street_address2',
+            'default_county'
+    )
+
+    ordering = ('user',)
+
+
+admin.site.register(Patron, ProfileAdmin)
