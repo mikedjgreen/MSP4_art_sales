a662e8d2cdc113b7c7d438f5b94a488fa342a862
Mike Green <m.d.j.green@ntlworld.com>
2021-03-18 14:53:26 +0000

    Added class methods to STRIPE webhooks

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('cache_checkout_data/',
          views.cache_checkout_data,
          name='cache_checkout_data'),
+    path('wh/', webhook, name='webhook'),     
 ]
