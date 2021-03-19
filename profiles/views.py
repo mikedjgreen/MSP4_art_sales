20d30dffc1cac364ed82c71a87ea2179ad3ec52e
Mike Green <m.d.j.green@ntlworld.com>
2021-03-19 09:36:16 +0000

    Styled allauth templates and connected profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Patron
 
 
 def profile(request):
     """ Display the user's profile. """
-
+    profile = get_object_or_404(Patron, user=request.user)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
