577e12aed74fafec59fe7d86ae90c4572c13d3a7
Mike Green <m.d.j.green@ntlworld.com>
2021-03-19 10:21:11 +0000

    Added Patron form and view

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import Patron
+from .forms import PatronForm
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(Patron, user=request.user)
+
+    if request.method == 'POST':
+        form = PatronForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = PatronForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
