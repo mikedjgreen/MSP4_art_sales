e081839580d22400f806f979bc177a71ecdaf8db
Mike Green <m.d.j.green@ntlworld.com>
2021-03-23 10:11:44 +0000

    Server 500 debug creating patron for mdjg

--- profiles/models.py
+++ profiles/models.py
@@ -30,6 +30,6 @@ def create_or_update_patron(sender, instance, created, **kwargs):
     Create or update the user profile (Patron)
     """
     #if created:
-        Patron.objects.create(user=instance)
+    Patron.objects.create(user=instance)
     # Existing users: just save the profile
     instance.patron.save()
